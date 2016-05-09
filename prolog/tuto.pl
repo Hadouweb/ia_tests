da9c7552ec96a2e4a10a0f2c8580d2dd52a8d95d
hadouweb <kengo14@hotmail.fr>
2016-05-10 00:55:04 +0200

    Ne fonctionne pas erreur de memory ...

--- prolog/tuto.pl
+++ prolog/tuto.pl
@@ -1,19 +1,80 @@
-plusGros('rhinocéros', 'cheval').
-plusGros('cheval', 'chien').
-plusGros('chien', 'chat').
-plusGros('chat', 'hamster').
+%************** Rules **************
+% Triangle
+nom(triangle) :-
+	ordre(3).
 
-estPlusGros(X, Y) :-
-	plusGros(X, Y).
+nom(triangleIsocele) :-
+	nom(triangle),
+	cotesEgaux(2).
 
-estPlusGros(X, Z) :-
-	plusGros(X, Y),
-	estPlusGros(Y, Z).
+nom(triangleRectangle) :-
+	nom(triangle),
+	angleDroit(oui).
 
-phrase(_) :-
-	estPlusGros(X, Y),
-	\+plusGros(X, Y),
-	write('Le '), write(X), write(' est plus gros que le '), write(Y), nl.
+nom(triangleRectangleIsoecele) :-
+	nom(triangleIsocele),
+	nom(triangleRectangle).
 
-phrases :-
-	findall(X, phrase(X), _).
+nom(triangleEquilatrel) :-
+	nom(triangle),
+	cotesEgaux(3).
+
+% Quadrilateres
+nom(quadrilateres) :-
+	ordre(4).
+
+nom(trapeze) :-
+	nom(quadrilateres),
+	cotesParalleles(2).
+
+nom(parallelogramme) :-
+	nom(quadrilatere),
+	cotesParalleles(4).
+
+nom(rectangle) :-
+	nom(parallelogramme),
+	angleDroit(oui).
+
+nom(losange) :-
+	nom(parallelogramme),
+	cotesEgaux(4).
+
+nom(carre) :-
+	nom(losange),
+	nom(rectangle).
+
+:- dynamic, memory/2.
+
+memory(Pred, _).
+
+ask(Pred, _, X) :-
+	dynamic, memory/2,
+	memory(Pred, X).
+
+ask(Pred, _, _) :-
+	memory(Pred, _),
+	!,
+	fail.
+
+ask(Pred, Question, X) :-
+	write(Question),
+	read(Y),
+	asserta(memory(Pred, Y)),
+	X == Y.
+
+cotesEgaux(X) :-
+	ask(cotesEgaux, 'Combien la figure a-t-elle de côtés égaux ? ', X).
+
+angleDroit(X) :-
+	ask(angleDroit, 'La figure possède-t-elle des angles droits (oui, non) ? ', X).
+
+cotesParalleles(X) :-
+	ask(cotesParalleles, 'Combien la figure a-t-elle de côtés parallèles (0, 2 ou 4) ? ', X).
+
+ordre(X) :-
+	ask(ordre, 'Combien de côtés ? ', X).
+
+solve :-
+	retractall(memory(_, _)),
+	findall(X, nom(X), R),
+	write(R).
